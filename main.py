956e49628ce8de3d366ad94abfe3d2cf28b9a836
Haku-Dell <g@kohaku.cc>
2018-03-30 17:29:45 +0800

    Init commit

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pickle
 import time
+import hashlib
 
 
 class Block:
-    def __init__(self, index, timestamp, transactions, previoid_hash):
+    def __init__(self, index, timestamp, transactions, previous_hash):
         self.index = index
         self.timestamp = timestamp
         self.transactions = transactions
-        self.previoid_hash = previoid_hash
+        self.previous_hash = previous_hash
 
-    def get_hash(self):
+    def block_hash(self):
         """
-        get hash of the block
+        create sha256 hash of the block
 
         :return:
         """
+        block_content = pickle.dumps(self)
+        return hashlib.sha256(block_content).hexdigest()
 
+
+
+def proof_of_work():
+    pass
 
 blockchain = []
 genesis_block = Block(0, time.time(), None, '0')
